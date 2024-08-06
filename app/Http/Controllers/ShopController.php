a6e5a0dc5e0daf73b8bee2f70aa3ed1ae926e614
Salahudin.cloud <ayubi54321@gmail.com>
2024-08-06 20:30:36 +0700

    update ShopController.php, navbar.blade.php, topBars.blade.php and web.php

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -6,9 +6,29 @@ use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    //
+    // shop
     public function shopIndex()
     {
         return view('shopMenu');
+    }
+
+
+    // detail shop
+    public function detailShopIndex()
+    {
+        return view('shopDetail');
+    }
+
+    // cart shop
+    public function chartShopIndex()
+    {
+        return view('shoppingChart');
+    }
+
+
+    // chekout shop
+    public function checkoutShopIndex()
+    {
+        return view('shoppingCheckout');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,10 @@ Route::get('/', [IndexController::class, 'index']);
 Route::get('shop', [ShopController::class, 'shopIndex']);
 
 Route::get('contact', [ContactController::class, 'contactIndex']);
+
+Route::get('detail/item/{itemSlug}', [ShopController::class, 'detailShopIndex']);
+// Route::get('detail', [ShopController::class, 'detailShopIndex']);
+
+Route::get('shop/chart', [ShopController::class, 'chartShopIndex']);
+
+Route::get('shop/chart/checkout', [ShopController::class, 'checkoutShopIndex']);
